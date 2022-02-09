17f88aac961bc362ca741dfa0839e133dc2b8cb9
Tim BOWMAN <tim@hellothisistim.com>
2022-02-09 15:40:14 -0500

    More clarity.

--- nukepedia/menu.py
+++ nukepedia/menu.py
@@ -11,21 +11,23 @@ nuke.pluginAddPath('./viewerInput1_1')
 m = nuke.menu('Nodes')
 user = os.getenv('USER')
 tm = m.findItem(user)
+
 if tm is not None:
 
+    nuke.tprint("Found menu: ", tm.name())
     # Label Dots
     try:
         import labelDots
         tm.addCommand('Label Dots', 'nuke.load("labelDots"), dotLabel()')
 
     except:
-        nuke.tprint('labelDots not found.')
+        nuke.tprint('labelDots not found. Skipping.')
 
-try:
-    import cycle_vlut
-    cycle_vlut.setup_menu()
-except:
-    nuke.tprint('cycle_vlut not found.')
+    try:
+        import cycle_vlut
+        cycle_vlut.setup_menu()
+    except:
+        nuke.tprint('cycle_vlut not found. Skipping.')
 
 
 nuke.tprint('ALL DONE from '+ os.path.realpath(__file__))
