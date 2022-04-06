658a4b470abde52b248d0746478e27ffb7ed6cba
Tim BOWMAN <tim@hellothisistim.com>
2022-04-06 16:16:44 -0400

    Clean up and consistency

--- init.py
+++ init.py
@@ -1,8 +1,8 @@
-# Copyright (c) 2009 The Foundry Visionmongers Ltd.  All Rights Reserved.
-
-## init.py
-## loaded by nuke before menu.py
+nuke.tprint('START: '+ os.path.realpath(__file__))
 
 nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./gizmos/3rdParty')
 nuke.pluginAddPath('./icons')
 nuke.pluginAddPath('./python')
+
+nuke.tprint('  END: '+ os.path.realpath(__file__))
