97432e01df92a9d6899ef6f6a3e1056f494c4d1c
Tim BOWMAN <tim@hellothisistim.com>
2022-06-01 11:18:56 -0400

    More info in messages

--- python/labelDots.py
+++ python/labelDots.py
@@ -6,6 +6,13 @@
 
 import nuke
 
+message = """Please select a dot.
+
+labelDots, by Howard Jones (<a href="http://www.nukepedia.com/python/nodegraph/labeldots/">Nukepedia</a>)
+
+This will label any dots to the same as the first higher level dot, with a label, found. If you label the top of your pipe "Main Mattes" and have successive dots underneath, select all and run this, then all your dots are relabelled to match."""
+
+
 def getParentNode(node):
     try:
         parentNode = node.input(0)
@@ -21,7 +28,7 @@ def getParentNode(node):
 
 def dotLabel():
         if not nuke.selectedNodes('Dot'):
-            nuke.message('Please select a dot.')
+            nuke.message(message)
             return
         else:
             nodes= nuke.selectedNodes('Dot')
