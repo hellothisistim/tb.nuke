7480955d2dbab8330a435cd191faf67b3d19fe9d
Tim BOWMAN <tim@hellothisistim.com>
2024-05-10 11:04:08 -0400

    Use Nuke Vector Matrix Toolset if installed

--- init.py
+++ init.py
@@ -5,4 +5,10 @@ nuke.pluginAddPath('./gizmos/3rdParty')
 nuke.pluginAddPath('./icons')
 nuke.pluginAddPath('./python')
 
+### Third-party tools
+
+# Nuke Vector Matrix Toolset: https://github.com/mapoga/nuke-vector-matrix/releases
+# Install in the tb.nuke folder.
+nuke.pluginAddPath('./nuke-vector-matrix-1.1.0')
+
 nuke.tprint('  END: '+ os.path.realpath(__file__))
