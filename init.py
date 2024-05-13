c72ec467cac8f3f34236755960731920b3064761
Tim BOWMAN <tim@hellothisistim.com>
2024-05-13 13:44:22 -0400

    Top-level 3rd party tool directory
    For tools that come with their own setup scripts.

--- init.py
+++ init.py
@@ -6,9 +6,15 @@ nuke.pluginAddPath('./icons')
 nuke.pluginAddPath('./python')
 
 ### Third-party tools
+# 
+# Many, many thanks to the authors for making these tools.
 
-# Nuke Vector Matrix Toolset: https://github.com/mapoga/nuke-vector-matrix/releases
-# Install in the tb.nuke folder.
-nuke.pluginAddPath('./nuke-vector-matrix-1.1.0')
+# Nuke Vector Matrix Toolset
+# by Mathieu Goulet-Aubin and Erwan Leroy
+# https://github.com/mapoga/nuke-vector-matrix/releases
+nuke.pluginAddPath('./3rdParty/nuke-vector-matrix-1.1.0')
+
+
+
 
 nuke.tprint('  END: '+ os.path.realpath(__file__))
