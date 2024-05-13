3f2f488f7b26f4d080e68cd73f0a2e0aa346396d
Tim BOWMAN <tim@hellothisistim.com>
2024-05-13 13:46:10 -0400

    Add Stamps 1.1.0

--- init.py
+++ init.py
@@ -14,7 +14,10 @@ nuke.pluginAddPath('./python')
 # https://github.com/mapoga/nuke-vector-matrix/releases
 nuke.pluginAddPath('./3rdParty/nuke-vector-matrix-1.1.0')
 
-
+# Stamps
+# by Adrian Pueyo and Alexey Kuchinski
+# https://github.com/adrianpueyo/Stamps/releases
+nuke.pluginAddPath("./3rdParty/Stamps-1.1.0")
 
 
 nuke.tprint('  END: '+ os.path.realpath(__file__))
