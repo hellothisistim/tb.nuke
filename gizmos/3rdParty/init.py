065bb80403db51ad54ae2efa945cb10974395dff
Tim BOWMAN <tim@hellothisistim.com>
2024-05-14 20:33:26 -0400

    Add gizmos from Adrian Pueyo

---
+++ gizmos/3rdParty/init.py
@@ -0,0 +1,14 @@
+nuke.tprint('START: '+ os.path.realpath(__file__))
+
+nuke.pluginAddPath('./apChroma_v1.0')
+nuke.pluginAddPath('./apDespill_v2.0')
+nuke.pluginAddPath('./apDirLight_v0.2')
+nuke.pluginAddPath('./apEdgePush_v1.2')
+nuke.pluginAddPath('./apGlow_v1.2')
+nuke.pluginAddPath('./aPMatte_v2.0')
+nuke.pluginAddPath('./apScreenGizmos_v0.5')
+nuke.pluginAddPath('./ColorSampler_v1.0')
+
+nuke.tprint('  END: '+ os.path.realpath(__file__))
+
+
--- gizmos/3rdParty/menu.py
+++ gizmos/3rdParty/menu.py
@@ -9,8 +9,17 @@ tm = m.findItem(user)
 if tm is not None:
     #nuke.tprint("Found menu: ", tm.name())
 
+    # Gizmos from Adrian Pueyo
+    # https://adrianpueyo.com/gizmos/
     tm.addCommand('Gizmos/Filter/apChroma', "nuke.createNode('apChroma')", icon="apChroma.png")
     tm.addCommand('Gizmos/Merge/apChromaMerge', "nuke.createNode('apChromaMerge')", icon = "apChroma.png")
+    tm.addCommand('Gizmos/Color/apDespill', "nuke.createNode('apDespill')", icon="apDespill.png")
+    tm.addCommand('Gizmos/Keyer/apDirLight', "nuke.createNode('apDirLight')", icon="apDirLight.png")
+    tm.addCommand('Gizmos/Filter/apEdgePush', "nuke.createNode('apEdgePush')", icon="apEdgePush.png")
+    tm.addCommand('Gizmos/Filter/apGlow', "nuke.createNode('apGlow')", icon="apGlow.png")
+    tm.addCommand('Gizmos/Other/aPMatte', "nuke.createNode('aPMatte')", icon="aPMatte.png")
+    tm.addCommand('Gizmos/Filter/apScreenClean', "nuke.createNode('apScreenClean')", icon="apScreenClean.png")
+    tm.addCommand('Gizmos/Filter/apScreenGrow', "nuke.createNode('apScreenGrow')", icon="apScreenGrow.png")
 
 else:
     nuke.tprint("User " + user + " does not have a menu. Not adding gizmos.")
